03943d6cb2e88c1667002848ece4bf049e1854fa
Parva Parmar <parva004cp@gmail.com>
2025-02-04 18:41:36 +0530

    "syntax pt2"

--- week-2/index.js
+++ week-2/index.js
@@ -1,5 +1,27 @@
-var a = 1;
-a = "harkirat";
-a = true;
+// var a = 1;
+// a = "harkirat";
+// a = true;
 
-console.log(a)
+// console.log(a)
+
+// // 1. Variables
+// let firstname = "deku";     // Variable that can be reassigned
+// const age = 30;        // Constant variable that cannot be reassigned
+// var isStudent = true;  // Older way to declare variables, function-scoped
+
+// console.log(firstname);
+// console.log(age);
+// console.log(isStudent);
+
+// // 2. Data types
+
+// let number = 42;             // Number
+// let string = "Hello World";  // String
+// let isActive = false;        // Boolean
+// let numbers = [1, 2, 3];     // Array
+
+// 3. Operators
+
+// let sum = 10 + 5;          // Arithmetic operator
+// let isEqual = (10 === 10); // Comparison operator
+// let isTrue = (true && false); // Logical operator
