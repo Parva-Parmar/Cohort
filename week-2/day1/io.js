d691745ca3a5ded88ae4eea1fa91725124d4e25d
Parva Parmar <parva004cp@gmail.com>
2025-02-06 16:45:24 +0530

    "async io"

--- week-2/day1/io.js
+++ week-2/day1/io.js
@@ -1,4 +1,22 @@
+// const fs = require("fs");
+
+// const contents = fs.readFileSync("a.txt", "utf-8");
+// console.log(contents);
+
+// const contents1 = fs.readFileSync("b.txt", "utf-8");
+// console.log(contents1);
+
+
 const fs = require("fs");
 
-const contents = fs.readFileSync("a.txt", "utf-8");
-console.log(contents);
+fs.readFile("a.txt", "utf-8", function (err, contents) {
+  console.log(contents);
+});
+
+fs.readFile("b.txt", "utf-8", function (err, contents) {
+  console.log(contents);
+});
+
+fs.readFile("a.txt", "utf-8", function (err, contents) {
+  console.log(contents);
+});
