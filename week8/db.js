96b6b4d5e5507a461ddd347b3ae4a08a8bc08630
Parva Parmar <parva004cp@gmail.com>
2025-04-14 18:43:29 +0530

    "create db"

--- week8/db.js
+++ week8/db.js
@@ -1,22 +1,43 @@
-const {schema, Schema, default: mongoose} = require("mongoose");
+const mongoose = require("mongoose");
+const course = require("./routes/course");
+const Schema = mongoose.Schema;
+const ObjectId = mongoose.Types.ObjectId;
 
 const userSchema = Schema({
-
+    email: {type: String , unique: true},
+    password: String,
+    firstName: String,
+    lastName: String
 });
 
 const adminSchema = Schema({
-
+    email: {type: String , unique: true},
+    password: String,
+    firstName: String,
+    lastName: String
 });
 
 const courseSchema = Schema({
-
+    title: String,
+    description: String,
+    price: String,
+    imageUrl: String,
+    createrId: ObjectId
 });
 
 const purchaseSchema = Schema({
-
+    userId: ObjectId,
+    courseId: ObjectId
 });
 
 const userModel = mongoose.Model("user", userSchema);
 const adminModel = mongoose.Model("admin", adminSchema);
 const courseModel = mongoose.Model("course", courseSchema);
 const purchaseModel = mongoose.Model("purchase", purchaseSchema);
+
+module.export = {
+    userModel,
+    adminModel,
+    courseModel,
+    purchaseModel
+}
