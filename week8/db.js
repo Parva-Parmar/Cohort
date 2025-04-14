0b2e1a414bb057ddb380c93397ad71224c623f18
Parva Parmar <parva004cp@gmail.com>
2025-04-14 19:20:24 +0530

    "added the schema"

--- week8/db.js
+++ week8/db.js
@@ -1,23 +1,24 @@
 const mongoose = require("mongoose");
-const course = require("./routes/course");
+console.log("connected to")
+mongoose.connect("mongodb+srv://parva04cp:u6lTTYAT8FCOPn82@cluster0.rq6ojvr.mongodb.net/coursera-app");
 const Schema = mongoose.Schema;
 const ObjectId = mongoose.Types.ObjectId;
 
-const userSchema = Schema({
+const userSchema = new Schema({
     email: {type: String , unique: true},
     password: String,
     firstName: String,
     lastName: String
 });
 
-const adminSchema = Schema({
+const adminSchema = new Schema({
     email: {type: String , unique: true},
     password: String,
     firstName: String,
     lastName: String
 });
 
-const courseSchema = Schema({
+const courseSchema = new Schema({
     title: String,
     description: String,
     price: String,
@@ -25,15 +26,15 @@ const courseSchema = Schema({
     createrId: ObjectId
 });
 
-const purchaseSchema = Schema({
+const purchaseSchema = new Schema({
     userId: ObjectId,
     courseId: ObjectId
 });
 
-const userModel = mongoose.Model("user", userSchema);
-const adminModel = mongoose.Model("admin", adminSchema);
-const courseModel = mongoose.Model("course", courseSchema);
-const purchaseModel = mongoose.Model("purchase", purchaseSchema);
+const userModel = mongoose.model("user", userSchema);
+const adminModel = mongoose.model("admin", adminSchema);
+const courseModel = mongoose.model("course", courseSchema);
+const purchaseModel = mongoose.model("purchase", purchaseSchema);
 
 module.export = {
     userModel,
