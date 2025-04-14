3ed2521d78b94a06e8cd19ffdf2f65d00eadddf6
Parva Parmar <parva004cp@gmail.com>
2025-04-14 19:25:10 +0530

    "done"

--- week8/index.js
+++ week8/index.js
@@ -2,10 +2,18 @@ const express = require('express');
 const { userRouter } = require("./routes/user");
 const {courseRouter} = require("./routes/course");
 const { adminRouter } = require("./routes/admin");
+const { default: mongoose } = require('mongoose');
 const app = express();
 
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/course", courseRouter);
 app.use("/api/v1/admin", adminRouter);
 
-app.listen(3000);
+
+async function main() {
+    await mongoose.connect("mongodb+srv://parva04cp:u6lTTYAT8FCOPn82@cluster0.rq6ojvr.mongodb.net/coursera-app")
+    app.listen(3000);
+    console.log("listning to port 3000");    
+}
+
+main()
