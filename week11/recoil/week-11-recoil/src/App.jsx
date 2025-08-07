2c15d28384c097b51d60dd491ef5beb17ccccadb
Parva Parmar <parva004cp@gmail.com>
2025-08-07 21:12:38 +0530

    restarted recoil

--- week11/recoil/week-11-recoil/src/App.jsx
+++ week11/recoil/week-11-recoil/src/App.jsx
@@ -1,53 +1,46 @@
-import { useState } from 'react';
+ 
 import './App.css'
 
 function App() {
-   
 
   return (
     <>
-      <RecoilRoot>
-        <Counter />
-      </RecoilRoot>
+      <Counter />
     </>
   )
 }
 
 function Counter(){
-  const [count,setCount] = useState(0);
+  const [count, setCount] = useState(0);  
+  return <div>
+    {count}
+    <Decrease setCount={setCount} />
+    <Increase setCount={setCount} />
+  </div>
+}
 
-  return (
-    <>
-      <CurrentCount count = {count}/>
-      <Increase setCount = {setCount}/>
-      <Decrease setCount = {setCount}/>
-    </>
+function Decrease(){
+
+  function decrease() {
+    setCount(c => c-1)
+  }
+  return(
+    <div>
+      <button onClick={decrease}>Decrease</button>
+    </div>
   )
 }
 
-function CurrentCount({count}){
-   return<div>
-    {count}
-   </div>
-}
+function Increase(){
 
-function Decrease({setCount}){
-  function decrease(){
-    setCount(c => c-1)
-  }
-  return <div>
-    <button onClick={decrease}>Decrease</button>
-  </div>
-}
-
-function Increase({setCount}){
-  function increase(){
+  function increase() {
     setCount(c => c+1)
   }
-
-  return <div>
-    <button onClick={increase}>Increase</button>
-  </div>
+  return(
+    <div>
+      <button onClick={increase}>Increase</button>
+    </div>
+  )
 }
 
 export default App
